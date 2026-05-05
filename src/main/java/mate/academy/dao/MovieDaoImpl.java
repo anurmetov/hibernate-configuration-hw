52cf30ac19df4800d673028d31fc1226803fac1a
artem <artemnurmetow@gmail.com>
2026-05-05 21:59:59 +0200

    - refactored handling of exception in methods

--- src/main/java/mate/academy/dao/MovieDaoImpl.java
+++ src/main/java/mate/academy/dao/MovieDaoImpl.java
@@ -24,7 +24,7 @@ public class MovieDaoImpl implements MovieDao {
             if (transaction != null) {
                 transaction.rollback();
             }
-            throw new DataProcessingException("Can't add movie");
+            throw new DataProcessingException("Can't add movie", e);
         } finally {
             if (session != null) {
                 session.close();
@@ -47,7 +47,7 @@ public class MovieDaoImpl implements MovieDao {
             if (transaction != null) {
                 transaction.rollback();
             }
-            throw new DataProcessingException("Cant find a movie");
+            throw new DataProcessingException("Cant find a movie", e);
         } finally {
             if (session != null) {
                 session.close();
