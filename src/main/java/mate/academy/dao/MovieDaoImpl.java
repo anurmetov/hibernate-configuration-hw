9afe52534127aca2d338139c44eaa3d23cd96beb
artem <artemnurmetow@gmail.com>
2026-05-05 21:28:57 +0200

    - fetch : implemeted singleton SessionFactory at util layer - fetch : implemented DAO Logic to add and get movies from DB - fetch : implemented ServiceLogic

--- src/main/java/mate/academy/dao/MovieDaoImpl.java
+++ src/main/java/mate/academy/dao/MovieDaoImpl.java
@@ -2,15 +2,31 @@ package mate.academy.dao;
 
 import java.util.Optional;
 import mate.academy.model.Movie;
+import mate.academy.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 public class MovieDaoImpl implements MovieDao{
     @Override
     public Movie add(Movie movie) {
-        return null;
+        SessionFactory sessionFactory = HibernateUtil.getSession();
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        session.persist(movie);
+        transaction.commit();
+        session.close();
+        return movie;
     }
 
     @Override
     public Optional<Movie> get(Long id) {
-        return Optional.empty();
+        SessionFactory sessionFactory = HibernateUtil.getSession();
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        Movie movie = session.find(Movie.class, id);
+        transaction.commit();
+        session.close();
+        return Optional.ofNullable(movie);
     }
 }
--- src/main/java/mate/academy/util/HibernateUtil.java
+++ src/main/java/mate/academy/util/HibernateUtil.java
@@ -1,4 +1,16 @@
 package mate.academy.util;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class HibernateUtil {
+    private static final SessionFactory session = initFactory();
+
+    private static SessionFactory initFactory() {
+        return new Configuration().configure().buildSessionFactory();
+    }
+
+    public static SessionFactory getSession() {
+        return session;
+    }
 }
